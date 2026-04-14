7a10c70104af5653a2b049f9d58787bf3437dbb4
bomjdev <anon@local>
2026-04-14 12:23:22 -0300

    NewMapKVFunc

--- map.go
+++ map.go
@@ -1,9 +1,15 @@
 package notstd
 
 func NewMapFunc[T any, K comparable, S ~[]T](slice S, keyFn KeyFn[T, K]) map[K]T {
-	s := make(map[K]T, len(slice))
+	return NewMapKVFunc(slice, keyFn, func(v T) T { return v })
+}
+
+type ValueFn[T, V any] func(T) V
+
+func NewMapKVFunc[T any, K comparable, V any, S ~[]T](slice S, keyFn KeyFn[T, K], valueFn ValueFn[T, V]) map[K]V {
+	s := make(map[K]V, len(slice))
 	for _, v := range slice {
-		s[keyFn(v)] = v
+		s[keyFn(v)] = valueFn(v)
 	}
 	return s
 }
